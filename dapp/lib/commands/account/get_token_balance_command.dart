ec6dd7880e0f98f519a0d17099cd041db22627a8
Atrixian Tech <hello@atrixian.com.br>
2022-01-18 13:29:17 -0300

    Update to refresh wallet when user change account and to load balance

--- dapp/lib/commands/account/get_token_balance_command.dart
+++ dapp/lib/commands/account/get_token_balance_command.dart
@@ -1,5 +1,8 @@
 import 'package:dapp/commands/base_command.dart';
 
 class GetTokenBalanceCommand extends BaseCommand {
-  Future execute() async {}
+  Future<BigInt> execute(String currentAccount) async {
+    return await appModel.tokenContract!
+        .call<BigInt>("balanceOf", [currentAccount]);
+  }
 }
--- dapp/lib/commands/account/set_current_account_command.dart
+++ dapp/lib/commands/account/set_current_account_command.dart
@@ -1,14 +1,15 @@
+import 'package:dapp/commands/account/get_token_balance_command.dart';
+import 'package:flutter_web3/flutter_web3.dart';
+
 import '../../dto/user_dto.dart';
 import '../base_command.dart';
 
 class SetCurrentAccountCommand extends BaseCommand {
   Future execute(String currentAccount) async {
-    if (appModel.user == null) {
-      var user = UserDto();
-      user.address = currentAccount;
-      appModel.user = user;
-    } else {
-      appModel.user!.address = currentAccount;
-    }
+    final balance = await GetTokenBalanceCommand().execute(currentAccount);
+
+    appModel.user = UserDto()
+      ..address = currentAccount
+      ..balance = EthUtils.formatEther(balance.toString());
   }
 }
