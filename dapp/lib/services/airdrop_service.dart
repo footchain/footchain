3b24d7fe16c9ff219f4d034405bff7f6176e456b
Atrixian Tech <hello@atrixian.com.br>
2022-01-26 15:22:29 -0300

    Update airdrop service to use constants

--- dapp/lib/services/airdrop_service.dart
+++ dapp/lib/services/airdrop_service.dart
@@ -1,14 +1,14 @@
 import 'dart:convert';
 
-import 'package:dapp/dto/response_dto.dart';
-import 'package:dapp/dto/signature_dto.dart';
+import '../dto/response_dto.dart';
+import '../dto/signature_dto.dart';
+import '../utils/constants.dart';
 import 'package:http/http.dart' as http;
 
 class AirdropService {
   Future<SignatureDto?> postRequest(
       String recaptchaToken, String address) async {
-    var url = Uri.parse(
-        'https://us-central1-footchain2022.cloudfunctions.net/requestAirdropPass');
+    var url = Uri.parse('${Constants.functionUri}requestAirdropPass');
     var response = await http
         .post(url, body: {'token': recaptchaToken, 'address': address});
     Map<String, dynamic> map = jsonDecode(response.body);
